281cc1db2a0164ec32107da66bc213ffd2d2d89a
Pavel <work.pbogdanov@gmail.com>
2021-12-06 00:12:13 +0300

    lesson01 at course "Swift" of Alex Skutarenko

---
+++ lesson01.playground/Contents.swift
@@ -0,0 +1,12 @@
+var nameStudent = "Pavel"
+var ageStudent = 24
+var education = "incomlete higher education"
+var university = "ISMART university"
+var universityAtCity = "Severodvinsk city"
+var studingTimeInYears = 3
+
+let greetings = "Hello! My name is \(nameStudent).\n"
+let presentEducation = "I have \(education) in \(university) at \(universityAtCity)\n"
+let studingTime = "I studied there \(studingTimeInYears) years.\n"
+
+print("\(greetings)\(presentEducation)\(studingTime)")
