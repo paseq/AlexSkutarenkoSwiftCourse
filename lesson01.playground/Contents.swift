53855715f9a7d9e58688c08beb71daaa6afec979
Pavel <work.pbogdanov@gmail.com>
2021-12-06 14:32:40 +0300

    added details

--- lesson01.playground/Contents.swift
+++ lesson01.playground/Contents.swift
@@ -1,12 +1,17 @@
-var nameStudent = "Pavel"
-var ageStudent = 24
-var education = "incomlete higher education"
-var university = "ISMART university"
-var universityAtCity = "Severodvinsk city"
-var studingTimeInYears = 3
+var firstNameStudent    = "Pavel"
+var lastNameStudent     = "Bogdanov"
+var ageStudent          = 24
+var education           = "incomlete higher education"
+var university          = "ISMART university"
+var universityAtCity    = "Severodvinsk city"
+var faculty             = "Faculty of Engineering Production Support"
+var speciality          = "Design engineer"
+var studingTimeInYears  = 3
 
-let greetings = "Hello! My name is \(nameStudent).\n"
-let presentEducation = "I have \(education) in \(university) at \(universityAtCity)\n"
-let studingTime = "I studied there \(studingTimeInYears) years.\n"
+let greetings           = "Hello! My name is \(firstNameStudent) \(lastNameStudent)."
+let presentEducation    = "I have \(education) in \(university) at \(universityAtCity)"
+let studingTime         = "I studied there \(studingTimeInYears) years on \(faculty) by proffesion \(speciality)."
 
-print("\(greetings)\(presentEducation)\(studingTime)")
+print(greetings)
+print(presentEducation)
+print(studingTime)
--- lesson02.playground/Contents.swift
+++ lesson02.playground/Contents.swift
@@ -1,3 +1,4 @@
-import UIKit
-
-var greeting = "Hello, playground"
+let bigNumber    = 1_234_567_89
+let octNumber    = 0o1234567
+let hexNumber    = 0x123456789ABCDEF
+let binaryNumber = 0b1111_1111
