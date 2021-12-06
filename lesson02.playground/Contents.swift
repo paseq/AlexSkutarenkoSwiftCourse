0d0ac67d6b2c673de33f628f1d633ce2fc6407b9
Pavel <work.pbogdanov@gmail.com>
2021-12-06 15:53:44 +0300

    some training with types of data

--- lesson02.playground/Contents.swift
+++ lesson02.playground/Contents.swift
@@ -1,4 +1,48 @@
-let bigNumber    = 1_234_567_89
+let intNumber    = 1_234_567_89
 let octNumber    = 0o1234567
 let hexNumber    = 0x123456789ABCDEF
 let binaryNumber = 0b1111_1111
+
+let intNumberMax = Int.max
+let intNumberMin = Int.min
+
+let unsignedIntMax : UInt = UInt.max
+let unsignedIntMin : UInt = UInt.min
+
+
+let intMax8 : Int8 = Int8.max
+let intMin8 : Int8 = Int8.min
+
+let unsignedIntMax8 : UInt8 = UInt8.max
+let unsignedIntMin8 : UInt8 = UInt8.min
+
+
+let intMax16 : Int16 = Int16.max
+let intMin16 : Int16 = Int16.min
+
+let unsignedIntMax16 : UInt16 = UInt16.max
+let unsignedIntMin16 : UInt16 = UInt16.min
+
+
+let intMax32 : Int32 = Int32.max
+let intMin32 : Int32 = Int32.min
+
+let unsignedIntMax32 : UInt32 = UInt32.max
+let unsignedIntMin32 : UInt32 = UInt32.min
+
+
+let intMax64 : Int64 = Int64.max
+let intMin64 : Int64 = Int64.min
+
+let unsignedIntMax64 : UInt64 = UInt64.max
+let unsignedIntMin64 : UInt64 = UInt64.min
+
+123.456e2   //123.456 * 10^2
+123.456e-2  //123.456 * 10^(-2)
+
+0x5
+0x5p2       //5 * 2^2
+0x5p-2      //5 * 2^(-2)
+
+typealias SomeNumber = Int
+let variable : SomeNumber = 10 
