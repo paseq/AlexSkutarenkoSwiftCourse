d3b380718847524f54a86081ddb0637ef60e8dd6
Pavel <work.pbogdanov@gmail.com>
2021-12-10 13:09:30 +0300

    added part 2 of homework of lesson04

--- lesson04.playground/Contents.swift
+++ lesson04.playground/Contents.swift
@@ -2,6 +2,9 @@
  LESSSON 04: OPTIONAL CHAINING
  */
 
+import Foundation
+import Security
+
 var value: Int? = nil
 
 if value == nil {
@@ -17,7 +20,7 @@ if let someValue = value {
 }
 
 /*
-                HOMEWORK
+ HOMEWORK
  */
 let fillString = "--------------------------------"
 let homeWorkPointer = "\(fillString)HOMEWORK\(fillString)"
@@ -29,11 +32,11 @@ let endPointer = fillString + fillString
 print(homeWorkPointer)
 
 /*
-    Part 1.
-
-    Create five string constants. Some of which have numbers and symbols,
-    some only numbers, some may be empty. It is necessary to find the sum
-    of these constants, casting them to the int type.
+ Part 1.
+ 
+ Create five string constants. Some of which have numbers and symbols,
+ some only numbers, some may be empty. It is necessary to find the sum
+ of these constants, casting them to the int type.
  */
 
 print(startFirstPartPointer)
@@ -62,7 +65,7 @@ if let tmp = Int(thirdNumber) {
 if let tmp = Int(fourthNumber) {
     sum += tmp
 }
-    
+
 if let tmp = Int(fifthNumber) {
     sum += tmp
 }
@@ -79,24 +82,54 @@ print("\nSum of constants is \(sum)")
 print(endPointer)
 
 /*
-    Part 2.
-    
-    You receive a tuple with three parameters from the server. 200-300
-    The first parameter is an status code of type Int, the second is message
-    of type String?, the third is an error message of type String?.
-    If status code in range 200 <= 300 display a message, otherwise
-    print error message.
-    
-    
-    
-    
+ Part 2.
  
+ You receive a tuple with three parameters from the server.
+ The first parameter is an status code of type Int, the second
+ is a message of type String?, the third is an error message of type
+ String?. If status code in range 200 <= 300 display a message, otherwise
+ display an error message.
  */
 
+print(startSecondPartPointer)
+
+//to uncomment lines for different checks
+
+let tupleFromServer: (codeStatus: Int, message: String?, errorMessage: String?)   = (300, "hello", "error")
+//let tupleFromServer: (codeStatus: Int, message: String?, errorMessage: String?)   = (300, nil, "error")
+//
+//let tupleFromServer: (codeStatus: Int, message: String?, errorMessage: String?)   = (301, "hello", "error")
+//let tupleFromServer: (codeStatus: Int, message: String?, errorMessage: String?)   = (301, "hello", nil)
+
+
+let missingMessage = "message is missing!"
+
+if tupleFromServer.codeStatus >= 200 && tupleFromServer.codeStatus <= 300 {
+    
+    if let tmp = tupleFromServer.message {
+        print(tmp)
+    } else {
+        print(missingMessage)
+    }
+    
+} else {
+    
+    if let tmp = tupleFromServer.errorMessage {
+        print(tmp + " with code status " + String(tupleFromServer.codeStatus))
+    } else {
+        print("error " + missingMessage)
+    }
+}
+
+
+
+print(endPointer)
+
 /*
-    Part 3.
+ Part 3.
  
-    You receive a tuple with three parameters from the server.
-    The first parameter is the student's name, the second parameter
-    is his car, the third parameter is the grade for the test.
+ You receive a tuple with three parameters from the server.
+ The first parameter is the student's name, the second parameter
+ is his car, the third parameter is the grade for the test.
  */
+
