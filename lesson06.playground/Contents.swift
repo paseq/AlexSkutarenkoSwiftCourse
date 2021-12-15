c087468f57b0f1957512f0eefd1168f0a5cd553a
Pavel <work.pbogdanov@gmail.com>
2021-12-15 22:12:39 +0300

    start homework of lesson06

--- lesson06.playground/Contents.swift
+++ lesson06.playground/Contents.swift
@@ -59,6 +59,7 @@ if firstString == secondString {
 firstString = "asd String"
 firstString.hasPrefix("asd")
 firstString.hasPrefix("hello")
+firstString.hasSuffix("String")
 
 
 /*
@@ -66,7 +67,7 @@ firstString.hasPrefix("hello")
  */
 
 let fillString = "--------------------------------"
-let homeWorkPointer = "\(fillString)HOMEWORK\(fillString)"
+let homeWorkPointer = "\n\(fillString)HOMEWORK\(fillString)"
 let startFirstPartPointer = "PART 1:\n"
 let startSecondPartPointer = "PART 2:\n"
 let startThirdPartPointer = "PART 3:\n"
@@ -76,17 +77,42 @@ print(homeWorkPointer)
 /*
  Part 1.
  
- 
+ Create five string constants. Some of which have numbers and symbols,
+ some only numbers, some may be empty. It is necessary to find the sum
+ of these constants, using ?? operator. First you need withdraw result
+ as String like a "5 + 2 + nil + 1 = 8" with string interpolation.
+ Second you need withdraw result with string concatenation.
  */
 print(startFirstPartPointer)
 
 
+let firstStringNumber:  String? = "123"
+let secondStringNumber: String? = nil
+let thirdStringNumber:  String? = ""
+let fourthStringNumber: String? = nil
+let fifthStringNumber:  String? = "321"
+
+let defaultValue = 0
+
+let firstNumber  = Int(firstStringNumber  ?? "") ?? defaultValue
+let secondNumber = Int(secondStringNumber ?? "") ?? defaultValue
+let thirdNumber  = Int(thirdStringNumber  ?? "") ?? defaultValue
+let fourthNumber = Int(fourthStringNumber ?? "") ?? defaultValue
+let fifthNumber  = Int(fifthStringNumber  ?? "") ?? defaultValue
+
+let sum = firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber
+
+print("\(firstNumber) + \(secondNumber) + \(thirdNumber) + \(fourthNumber) + \(fifthNumber) = \(sum)")
+
+print(String(firstString) + String(secondString))
 
 print(endPointer)
 /*
  Part 2.
  
- 
+ Select a five Unicode characters. Write them on a string.
+ Count the length of the string, first with ".count", then
+ using "NSString and length".
  */
 print(startSecondPartPointer)
 
@@ -96,6 +122,9 @@ print(endPointer)
 /*
  Part 3.
  
+ Using cycle "for" withdraw english alphabet with uncapital
+ symbols. Create a variable with symbol of alphabet and find
+ the index of this symbol.
  
  */
 print(startThirdPartPointer)
