3f0efcb41fb1c84097a8ae13f246ab578e7d9025
Pavel <work.pbogdanov@gmail.com>
2021-12-18 22:30:16 +0300

    start lesson08

---
+++ lesson08.playground/Contents.swift
@@ -0,0 +1,50 @@
+/*
+             LESSSON08: Dictionaries
+ */
+
+
+
+/*
+             HOMEWORK
+ */
+
+let fillString = "--------------------------------"
+let homeWorkPointer = "\(fillString)HOMEWORK\(fillString)"
+let startFirstPartPointer = "PART 1:\n"
+let startSecondPartPointer = "PART 2:\n"
+let startThirdPartPointer = "PART 3:\n"
+let endPointer = fillString + fillString
+
+print(homeWorkPointer)
+/*
+ Part 1.
+ 
+ 
+ */
+print(startFirstPartPointer)
+
+
+
+print(endPointer)
+/*
+ Part 2.
+ 
+ 
+ */
+print(startSecondPartPointer)
+
+
+
+print(endPointer)
+/*
+ Part 3.
+ 
+ 
+ */
+print(startThirdPartPointer)
+
+
+    
+print(endPointer)
+
+
