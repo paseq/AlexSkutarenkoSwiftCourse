f3058ad715ec56ce297bed59edb08d8ed7928395
Pavel <work.pbogdanov@gmail.com>
2021-12-19 23:41:24 +0300

    work of lesson08

--- lesson08.playground/Contents.swift
+++ lesson08.playground/Contents.swift
@@ -2,7 +2,58 @@
              LESSSON08: Dictionaries
  */
 
+var dictSampleFirst = [0 : "zero", 1 : "first", 2 : "second"]
 
+dictSampleFirst[0]
+dictSampleFirst[1]
+dictSampleFirst[2]
+
+dictSampleFirst.count
+dictSampleFirst.isEmpty
+
+dictSampleFirst[3] = "third" //create write of dictionary
+dictSampleFirst
+
+dictSampleFirst[0] = "replaced value" //replace value
+dictSampleFirst
+
+dictSampleFirst[0] = "zero"
+
+dictSampleFirst.keys
+dictSampleFirst.values
+
+let replacedValue = dictSampleFirst.updateValue("replaced value", forKey: 0) // replace the value but keep the old value
+replacedValue
+dictSampleFirst
+
+dictSampleFirst[0] = "zero"
+dictSampleFirst
+
+let appendedValue = dictSampleFirst.updateValue("appended value", forKey: 4) //not exist key. creating new key and return nil as old value
+appendedValue
+dictSampleFirst
+
+let sampleValue = dictSampleFirst[0]  //dictSampleFirst[0] return optional value
+
+dictSampleFirst[4] = nil  //delete write of dictionary
+dictSampleFirst
+
+dictSampleFirst.removeValue(forKey: 0) //return deleted value.
+dictSampleFirst
+
+dictSampleFirst = [:] //remove all keys
+
+dictSampleFirst = [0 : "zero",
+                   1 : "first",
+                   2 : "second"]
+
+for key in dictSampleFirst.keys {
+    print("key = \(key), value = \(dictSampleFirst[key]!)")
+}
+
+for (key, value) in dictSampleFirst {
+    print("key = \(key), value = \(value)")
+}
 
 /*
              HOMEWORK
