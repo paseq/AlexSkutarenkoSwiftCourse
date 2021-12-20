8d671408cecb94ab8375a786d942bc7b3c1fcf2c
Pavel <work.pbogdanov@gmail.com>
2021-12-20 16:52:12 +0300

    added part 2 of homework of lesson08

--- lesson08.playground/Contents.swift
+++ lesson08.playground/Contents.swift
@@ -2,6 +2,8 @@
              LESSSON08: Dictionaries
  */
 
+import Foundation
+
 var dictSampleFirst = [0 : "zero", 1 : "first", 2 : "second"]
 
 dictSampleFirst[0]
@@ -74,11 +76,18 @@ print(homeWorkPointer)
 /*
  Part 1.
  
- 1.1.   Create a dictionary as student diary, where name and lastname is key of dictionary, grade for test it is value of key.
+ 1.1.   Create a dictionary as student diary, where
+        name and lastname is key of dictionary, grade
+        for test it is value of key.
+ 
  1.2.   Some students have changed grades.
+ 
  1.3.   To add new students to the dictionary.
+ 
  1.4.   Delete some students.
- 1.5.   Calculate the average of students grades and general score for all students.
+ 
+ 1.5.   Calculate the average of students grades and
+        general score for all students.
  */
 print(startFirstPartPointer)
 
@@ -148,7 +157,6 @@ for (_, value) in dictionaryOfStudents {
     
     sumOfGrades += value
     
-    
 }
 
 averageOfGrades = sumOfGrades / dictionaryOfStudents.count
@@ -161,22 +169,94 @@ print(endPointer)
 /*
  Part 2.
  
+ 2.1.   Create a dictionary where key is month,
+        number of days in this month is value.
+        
+        Withdraw in console a copule 'month-number
+        of days' of days:
  
+ 2.2.   First with help tuples.
+ 
+ 2.3.   Second with help array of keys and getting
+        out values.
  */
 print(startSecondPartPointer)
 
+
+
+
+//  PART 2.1
+print("\n2.1:\n")
+
+
+let dictionaryOfMonths = [ "January" : 31,
+                          "February" : 28,
+                             "March" : 31,
+                             "April" : 30,
+                               "May" : 31,
+                              "June" : 30,
+                              "July" : 31,
+                            "August" : 31,
+                         "September" : 30,
+                           "October" : 31,
+                          "November" : 30,
+                          "December" : 31]
+
+print(dictionaryOfMonths)
+
+
+//  PART 2.2
+print("\n2.2:\n")
+
+for (month, numberOfDays) in dictionaryOfMonths {
+    
+    print("\t\(month) has \(numberOfDays).")
+    
+}
+
+
+//  PART 2.3
+print("\n2.3:\n")
+
+for key in dictionaryOfMonths.keys {
+    
+    print("\t\(key) has \(dictionaryOfMonths[key]!).")
+    
+}
 
 
 print(endPointer)
 /*
  Part 3.
  
+ Using part 3 of homework of lesson05, create a
+ dictionary where keys is number of cell like 'a5'
+ or 'b8', and values of this keys is true if cell
+ with these number white and false if black.
  
  */
 print(startThirdPartPointer)
 
+var chessCells: [String : Bool] = [:]
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h" ]
 
+for (x, letter) in letters.enumerated() {
     
+    for y in 0..<letters.count {
+        
+        var value = true
+        
+        if ((x + 1) * (y + 1)) % 2 != 0 {
+            value = false
+        }
+
+        chessCells.updateValue(value, forKey: letter + "\(x + 1)")
+    }
+    
+}
+
+chessCells
+
 print(endPointer)
 
 
