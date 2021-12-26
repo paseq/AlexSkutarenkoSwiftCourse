f47f711b6177340e64cde284aff9f130b656ee9c
Pavel <work.pbogdanov@gmail.com>
2021-12-26 19:41:33 +0300

    refactored homework of lesson09

--- lesson09.playground/Contents.swift
+++ lesson09.playground/Contents.swift
@@ -30,19 +30,21 @@ switch age {
         break
     case 51, 53:
         print("51, 53")
+
     default: print("default")
 }
 
 let name = "Alex"
 
 switch name {
-    
-    case "Alex" where age < 50:   //'where' allows add condition
-        print("Name = \(name), Age = \(age)")
-    case "Alex" where age >= 50:
-        print(">= 50")
-    default:
-        break
+
+case "Alex" where age < 50:   //'where' allows add condition
+    print("Name = \(name), Age = \(age)")
+case "Alex" where age >= 50:
+    print(">= 50")
+
+default:
+    break
 }
 
 let optional: Int? = 5
@@ -52,43 +54,46 @@ if let a = optional {    //optional binding
 }
 
 switch (name, age) {
-    
-    case ("Alex", 60):
-        print("\(name) \(age)")
-    case (_, let number) where number >= 65 && number <= 70:   //value binding
-        print("value binding")
-    case ("Alex", _):
-        print("Hi, \(name)")
-    default:
-        break
+
+case ("Alex", 60):
+    print("\(name) \(age)")
+case (_, let number) where number >= 65 && number <= 70:   //value binding
+    print("value binding")
+case ("Alex", _):
+    print("Hi, \(name)")
+
+default:
+    break
 }
 
 let point = (5, -6)
 
 switch point {
-    
-    case let (x, y) where x == y:
-        print("x == y")
-    case let (x, y) where x == -y:
-        print("x == -y")
-    case let (_, y) where y < 0:
-        print("y < 0")
-    default:
-        break
+
+case let (x, y) where x == y:
+    print("x == y")
+case let (x, y) where x == -y:
+    print("x == -y")
+case let (_, y) where y < 0:
+    print("y < 0")
+
+default:
+    break
 }
 
 let array: [CustomStringConvertible] = [5, 5.4, Float(5.4)]
 
 switch array[0] {
-    
-    case _ as Int:
-        print("Int")
-    case _ as Double:
-        print("Double")
-    case _ as Float:
-        print("Float")
-    default:
-        break
+
+case _ as Int:
+    print("Int")
+case _ as Double:
+    print("Double")
+case _ as Float:
+    print("Float")
+
+default:
+    break
 }
               
 /*
@@ -132,16 +137,17 @@ for char in someString {
     
     switch char {
         
-        case _ where char.isNumber:
-            chars.numbers += 1
-        case _ where !char.isLetter:
-            chars.symbols += 1
-        case _ where char.isLetter:
-            let vowelsArray = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"]
+    case _ where char.isNumber:
+        chars.numbers += 1
+    case _ where !char.isLetter:
+        chars.symbols += 1
+    case _ where char.isLetter:
+        let vowelsArray = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"]
 
-            vowelsArray.contains(String(char)) ? (chars.vowels += 1) : (chars.consonants += 1)
-        default:
-            break
+        vowelsArray.contains(String(char)) ? (chars.vowels += 1) : (chars.consonants += 1)
+
+    default:
+        break
     }
 }
 
@@ -168,17 +174,18 @@ print(startSecondPartPointer)
 let oldAge = 0
 
 switch oldAge {
+
+case 0...1: print("Age of infancy")
+case 1...3: print("Age of early")
+case 3...7: print("Preschool age")
+case 7...12: print("Primary school age")
+case 12...15: print("Early adolescence")
+case 15...18: print("Adolescence")
+case 18...35: print("Youth")
+case 35...60: print("Maturity")
+case 60...100: print("Old age")
     
-    case 0...1: print("Age of infancy")
-    case 1...3: print("Age of early")
-    case 3...7: print("Preschool age")
-    case 7...12: print("Primary school age")
-    case 12...15: print("Early adolescence")
-    case 15...18: print("Adolescence")
-    case 18...35: print("Youth")
-    case 35...60: print("Maturity")
-    case 60...100: print("Old age")
-    default: break
+default: print("Invalid value!")
 }
 
 
@@ -186,27 +193,27 @@ print(endPointer)
 /*
      Part 3.
      
-     У вас есть имя отчество и фамилия студента (русские буквы). Имя начинается с А или О, то обращайтесь к студенту по имени, если же
-     нет,то если у него отчество начинается на В или Д, то обращайтесь к нему по имени и отчеству, если же опять нет, то в случае если
-     фамилия начинается с Е или З, то обращайтесь к нему только по фамилии. В противном случае обращайтесь к нему по полному имени.
+    You have the students full name (russian letters). If the name
+    begins with the letters "А" or "О", call his by name, if the
+    middle name begins with the letters "В" or "Д", call him by name
+    and last name. If last name begins with letter "Е" or "З", call
+    him only by last name. Otherwise, call his by his full name.
  */
 print(startThirdPartPointer)
 
 
-let personName = (name: "Тлександр", lastName: "Тлизаров", patronymic: "Тладимирович")
+let personName = (name: "Тлександр", lastName: "Тлизаров", middleName: "Тладимирович")
 
 switch personName {
-    
-    case (let name, _, _) where name.hasPrefix("А") || name.hasPrefix("О"):
-        print("Hello, \(name).")
-    
-    case (let name, _, let patronymic) where patronymic.hasPrefix("В") || patronymic.hasPrefix("Д"):
-        print("Hello, \(name) \(patronymic).")
-    
-    case (_, let lastName, _) where lastName.hasPrefix("Е") || lastName.hasPrefix("З"):
-        print("Hello, \(lastName).")
-    
-    default: print("Hello, \(personName.name) \(personName.lastName) \(personName.patronymic).")
+
+case (let name, _, _) where name.hasPrefix("А"): fallthrough
+case (let name, _, _) where name.hasPrefix("О"): print("Hello, \(name).")
+case (let name, _, let middleName) where middleName.hasPrefix("В"): fallthrough
+case (let name, _, let middleName) where middleName.hasPrefix("Д"): print("Hello, \(name) \(middleName).")
+case (_, let lastName, _) where lastName.hasPrefix("Е"): fallthrough
+case (_, let lastName, _) where lastName.hasPrefix("З"): print("Hello, \(lastName).")
+
+default: print("Hello, \(personName.name) \(personName.lastName) \(personName.middleName).")
 }
 
 
@@ -214,8 +221,10 @@ print(endPointer)
 /*
     Part 4.
  
-    Представьте что вы играете в морской бои и у вас осталось некоторое количество кораблей на поле 10Х10 (можно буквы и цифры,
-    а можно только цифры). Вы должны создать свитч, который примет тюпл с координатой и выдаст один из вариантов: мимо, ранил, убил.
+    Imagine that you playing in sea war and you have some warships
+    on battlefield with size 10x10. You should create a switch, that
+    get the number of row and letter of column and withdraw: miss,
+    wounded or killed.
  */
 print(startFourthPartPointer)
 
@@ -230,14 +239,15 @@ if !letters.contains(coords.letter) || coords.digit < 1 || coords.digit > 10 {
 
 switch coords {
 
-    case ("d", 3...5):
-        print("Wounded!")
-    case ("f", 2...3):
-        print("Wounded!")
-    case ("b", 4), ("g", 7), ("f", 8):
-        print("Killed!")
-    default:
-        print("Miss!")
+case ("d", 3...5):
+    print("Wounded!")
+case ("f", 2...3):
+    print("Wounded!")
+case ("b", 4), ("g", 7), ("f", 8):
+    print("Killed!")
+    
+default:
+    print("Miss!")
 }
 
 
